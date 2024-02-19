9c0a158227bad48cdc55415a436da19c359ba130
Sagar N.R <130398768+Sagarnr1997@users.noreply.github.com>
2024-02-20 00:53:28 +0530

    Update ready_to_deploy.py

--- ready_to_deploy.py
+++ ready_to_deploy.py
@@ -99,6 +99,15 @@ q=power_conspution.iloc[112903:112975,1]
 power_conspution.iloc[112999:113071,1]=q
 power_conspution = power_conspution.dropna()
 
+X = power_conspution[['Day', 'year', 'month', 'hour', 'name_of_week', 'season', 'holiday']]
+Y = power_conspution[['PJMW_MW']]
+        
+model = xgb.XGBRegressor(base_score=1, booster='gbtree',    
+                                 n_estimators=1000,
+                                 objective='reg:squarederror',
+                                 max_depth=10,
+                                 learning_rate=0.1, gamma=1)
+model.fit(X, Y, verbose=100)
 
 with st.container():
     tabs =["About Project and their objectives","User inputs and prediction"]
@@ -224,16 +233,6 @@ with st.container():
         # Creating the features
         power_conspution['season'] = LabelEncoder().fit_transform(power_conspution['season'])
         power_conspution['holiday'] = LabelEncoder().fit_transform(power_conspution['holiday'])
-        
-        X = power_conspution[['Day', 'year', 'month', 'hour', 'name_of_week', 'season', 'holiday']]
-        Y = power_conspution[['PJMW_MW']]
-        
-        model = xgb.XGBRegressor(base_score=1, booster='gbtree',    
-                                 n_estimators=1000,
-                                 objective='reg:squarederror',
-                                 max_depth=10,
-                                 learning_rate=0.1, gamma=1)
-        model.fit(X, Y, verbose=100)
         
         if st.button("Predict"):   
             prediction = model.predict(df)
